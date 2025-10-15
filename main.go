acd7d3830b63ef1d4ce2804c5c8816a75c2e7294
Samit Koyom <samit90daytalk@hotmail.com>
2025-10-16 00:11:30 +0700

    update main.go

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "Pong 🏓",
+			"message": "Pong Pong Pong 🏓",
 			"status":  "success",
 		})
 	})
